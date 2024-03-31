dcfb93924911fbac69ea91f6b1e2d3e5f2a48cfb
Javlonbek Abdurasulov <javlonbeabr41@gmail.com>
2024-03-31 18:00:19 +0500

    hosting settings

--- src/i18n.js
+++ src/i18n.js
@@ -5,7 +5,7 @@ import { initReactI18next } from "react-i18next";
 const getCurrentHost =
   import.meta.env.MODE === "development"
     ? "http://localhost:5173"
-    : "hosUrl here";
+    : "https://javstrux.netlify.app/";
 
 i18n
   .use(i18nBackend)
